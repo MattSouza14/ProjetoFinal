d8ed278c25a34582427dc99aca0a6a6d920ac053
magicianLucas <lucastelesieqs@gmail.com>
2024-07-15 17:06:41 -0300

    Componente Logo

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,12 @@
+import logo from '/src/assets/logo.png'
+
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="Logo" />
+        </div>
+    )
+}
+
+export default Logo
