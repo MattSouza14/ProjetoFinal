579256546b93ed4852a272ed0b9154d483268481
JOAO PEDRO DE OLIVEIRA MATIAS <JOAO6081@DIGITALCOLLEGE.LOCAL>
2024-07-18 16:25:33 -0300

    carddestaque

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,22 @@ import ColecoesDestaque2 from './Components/ColecoesDestaque2'
 import BanerPrincipal from './Components/BanerPrincipal.jsx'
 import './Components/BarnerPrincipal.css'
 
+import DestaqueColecaoIcone from './Components/DestaqueColecaoIcones'
 
 
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  let produtos = [{
+  let listaprodutos = [{
+    nome: 'Tênis',
+    descricao:'K-Swiss V8 - Masculino',
+    oldPrice: '$200',
+    newPrice:'$149.9',
+    desconto:'%30 OFF',
+    imagem: 'src/assets/tenisCardUm.png'
+  },
+  {
     nome: 'Tênis',
     descricao:'K-Swiss V8 - Masculino',
     oldPrice: '$200',
@@ -43,8 +52,8 @@ function App() {
     desconto:'%30 OFF',
     imagem: 'src/assets/tenisCardUm.png'
   }
-  
-  
+
+
 ]
 let produtosNoDiscount = [
   {
@@ -139,16 +148,16 @@ let produtosNoDiscount = [
   imagem: 'src/assets/tenisCardUm.png'
 },
 ]
-  
+
 
   return (
   <>
     <Header></Header>
- 
-    <ColecoesDestaque2></ColecoesDestaque2>
-    <ProductFilter/>
-    <ListProductCard produtos={produtos}/>
-    <ListProductCard produtosNoDiscount={produtosNoDiscount}/>
+    <DestaqueColecaoIcone ListaProdutos={listaprodutos}/>
+    {<ColecoesDestaque2></ColecoesDestaque2>}
+    {<ProductFilter/>}
+    {<ListProductCard produtos={produtos}/>}
+    {<ListProductCard produtosNoDiscount={produtosNoDiscount}/>}
     
 
     <Footer></Footer>
