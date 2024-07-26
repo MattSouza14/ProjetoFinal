3c78f62478a8e90281356d1234f115f1424f31e9
Vicore123 <viniciuslimaalves123@gmail.com>
2024-07-26 13:44:38 -0300

    Polimento do codigo

--- src/pages/PaginaNaoEncontrada.jsx
+++ src/pages/PaginaNaoEncontrada.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from 'styled-components'; // Importe correto do styled-components
-import './PaginaNaoEncontrada.css'; // Importe correto do arquivo CSS local
 
-// Estilização usando styled-components (opcional)
+let StyledTitle = styled.div`
+
+display: flex;
+justify-content: center;
+padding: 50px;
+background-color: #F9F8FE;
+
+`
 
 export default function PaginaNaoEncontrada() {
   return (
-    <TituloContainer>
-      <Titulo>Página não encontrada</Titulo>
-    </TituloContainer>
+    <StyledTitle>
+      <h1>Página não encontrada : (</h1>
+    </StyledTitle>
   );
 }
 
